aeb07090a9811b8330e1467acb47256138a5dbc3
Bobbaru <bobbaru_krueger@yahoo.com>
2024-08-14 00:25:41 +0300

    handle next auth session and jwt callbacks

--- webapp/src/lib/auth.ts
+++ webapp/src/lib/auth.ts
@@ -1,9 +1,57 @@
 import authConfig from "@/lib/auth.config";
+import { getUserById } from "@/lib/data";
+import db from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
-import db from "./db";
+import { UserRole } from "@prisma/client";
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's role. */
+      role: UserRole;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id!);
+
+    //   if (!existingUser || !existingUser.emailVerified) return false;
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) session.user.id = token.sub;
+
+      if (token.role && session.user)
+        session.user.role = token.role as UserRole;
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
