51fd1922e715a2d0ff0a3b12cc88c9a5667bcf33
Bobbaru <bobbaru_krueger@yahoo.com>
2024-08-19 01:15:28 +0300

    document server functions

--- webapp/src/actions/new-verification.ts
+++ webapp/src/actions/new-verification.ts
@@ -2,13 +2,31 @@ import db from "@/lib/db";
 import { getUserByEmail } from "@/lib/data";
 import { getVerificationTokenByToken } from "@/lib/data";
 
+/**
+ * **{@linkcode newVerification} server function**
+ *
+ * 1. Get verification token. See {@linkcode getVerificationTokenByToken}
+ * 2. If Token does not exist return `{ error: "Token does not exist!" }`
+ * 3. Check if Token is expired. If yes return `{ error: "Token has expired!" }`
+ * 4. Get user by email. See {@linkcode getUserByEmail}
+ * 5. If user does __not__ exists return `{ error: "Email does note exist!" }`
+ * 6. Update the user
+ * 7. Delete verification Token
+ * 8. Return `{ success: ""Email verified!" }`
+ *
+ * @param {string} token
+ * @yields Returns a `Promise` that returns an `Object` with success and error messages
+ */
 export const newVerification = async (token: string) => {
+  // 1
   const existingToken = await getVerificationTokenByToken(token);
 
+  // 2
   if (!existingToken) {
     return { error: "Token does not exist!" };
   }
 
+  // 3
   const hasExpired = new Date(existingToken.expires) < new Date();
 
   if (hasExpired)
@@ -16,13 +34,16 @@ export const newVerification = async (token: string) => {
       error: "Token has expired!",
     };
 
+  // 4
   const existingUser = await getUserByEmail(existingToken.email);
 
+  // 5
   if (!existingUser)
     return {
       error: "Email does not exist!",
     };
 
+  // 6
   await db.user.update({
     where: {
       id: existingUser.id,
@@ -33,12 +54,14 @@ export const newVerification = async (token: string) => {
     },
   });
 
+  // 7
   await db.verificationToken.delete({
     where: {
       id: existingToken.id,
     },
   });
 
+  // 8
   return {
     success: "Email verified!",
   };
